3446850828a70c7efdbd9c50df053dd76759328d
JorgeBB22 <elduro300904@gmail.com>
2024-08-22 16:20:13 -0400

    Add files via upload

---
+++ Tabuada.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    printf("Tabula de 1 a 9\n");
+    printf("-----------------\n");
+
+    for (i = 1; i <- 9; i++) {
+        
+        for (j = 1 ; j <= 9; j++) {
+            printf("%4d", i * j);
+
+        }
+        
+        printf ("\n");
+    }   
+        return 0;
+}        
+            
+        
+    
